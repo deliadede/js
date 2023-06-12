c700938ca3fe2162cf7bf1d67851fee2afa6c186
deliadede <delia.danciu@lepoles.org>
2023-06-12 09:52:47 +0200

    first commit

---
+++ js/05.js
@@ -0,0 +1,29 @@
+// alert('test');
+
+// Structure de base IF
+
+if (true){// par défaut la condition à vérifier le IF vérifie si elle est vraie
+
+    //.... code .... 
+
+}
+
+var delia = 50;
+if (delia < 50){
+    console.log("nb1 est bien inférieur à 50");
+}
+
+// ELSE
+
+if (true){
+//.....code si VRAI ....
+} else {
+//..... code si FAUX
+}
+
+
+if (delia > 50){
+    console.log("nb1 est bien supérieur à 50");
+} else{
+    console.log("nb1 est inférieur à 50");
+}
